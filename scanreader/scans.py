aceaef142ffd0fe8af13f53bbdc0697ecbd25129
Tobias Rose <tobias.rose@ukbonn.de>
2023-03-20 16:53:25 +0100

    Update scans.py - motor position is not axesposition

--- scanreader/scans.py
+++ scanreader/scans.py
@@ -241,14 +241,14 @@ class BaseScan():
     def motor_position_at_zero(self):
         """ Motor position (x, y and z in microns) corresponding to the scan's (0, 0, 0)
         point. For non-multiroi scans, (x=0, y=0) marks the center of the FOV."""
-        match = re.search(r'hMotors\.motorPosition = (?P<motor_position>.*)', self.header)
+        match = re.search(r'hMotors\.axesPosition = (?P<motor_position>.*)', self.header)
         motor_position = matlabstr2py(match.group('motor_position'))[:3] if match else None
         return motor_position
 
     @property
     def initial_secondary_z(self):
         """ Initial position in z (microns) of the secondary motor (if any)."""
-        match = re.search(r'hMotors\.motorPosition = (?P<motor_position>.*)', self.header)
+        match = re.search(r'hMotors\.axesPosition = (?P<motor_position>.*)', self.header)
         if match:
             motor_position = matlabstr2py(match.group('motor_position'))
             secondary_z = motor_position[3] if len(motor_position) > 3 else None
